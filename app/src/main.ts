58b33a8de79a4cca47146ba0a1d95255b01d037e
Kazuya-Pum <kazuya.pum@gmail.com>
2021-07-08 01:23:06 +0900

    vuetify

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
+import vuetify from "./plugins/vuetify";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(router).use(store).use(vuetify).mount("#app");
