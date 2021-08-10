5b0ac711704be9980352f379ee607ff15b11bb64
Kazuya-Pum <30061332+Kazuya-Pum@users.noreply.github.com>
2021-08-10 22:52:15 +0900

    firebase sdk

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,8 +4,20 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import firebase from "firebase/app";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyApVTxcMN32inU6jLawg0iXUG5TKtQDQP0",
+  authDomain: "kazuya-pum.firebaseapp.com",
+  projectId: "kazuya-pum",
+  storageBucket: "kazuya-pum.appspot.com",
+  messagingSenderId: "968408286992",
+  appId: "1:968408286992:web:291e5d339cb7adc1cec795",
+  measurementId: "G-35R9HJ1L3Z",
+};
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
